59ed853612bfa88ff852a5eb1afc74a7b08abb57
nasrulilham <91175018+nasrulilham@users.noreply.github.com>
2024-04-23 23:09:50 +0700

    Long method-TaxFunction
    Mengganti pesanr error dengan melemparkan exception.

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -20,7 +20,7 @@ public class TaxFunction {
 		int tax = 0;
 		
 		if (numberOfMonthWorking > 12) {
-			System.err.println("More than 12 month working per year");
+			throw new IllegalArgumentException("More than 12 months working per year");
 		}
 		
 		if (numberOfChildren > 3) {
